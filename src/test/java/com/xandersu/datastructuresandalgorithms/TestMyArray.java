70179742dc43e1e4d48197e36be71bc60482fe24
suxun <suxun@gotokeep.com>
2018-09-17 23:08:49 +0800

    add myArray

---
+++ src/test/java/com/xandersu/datastructuresandalgorithms/TestMyArray.java
@@ -0,0 +1,18 @@
+package com.xandersu.datastructuresandalgorithms;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: suxun
+ * @Date: 2018/9/17 23:06
+ * @Description:
+ */
+@SpringBootTest
+public class TestMyArray {
+
+    @Test
+    public  void test1(){
+        System.out.println(1);
+    }
+}
