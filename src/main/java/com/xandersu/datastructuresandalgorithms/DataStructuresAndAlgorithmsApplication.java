2ef98fcb60535cf4f2052273b2fc26e261871fde
xandersu <xandersu666@gmail.com>
2018-09-17 22:50:46 +0800

    Add files via upload

---
+++ src/main/java/com/xandersu/datastructuresandalgorithms/DataStructuresAndAlgorithmsApplication.java
@@ -0,0 +1,12 @@
+package com.xandersu.datastructuresandalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresAndAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresAndAlgorithmsApplication.class, args);
+	}
+}
