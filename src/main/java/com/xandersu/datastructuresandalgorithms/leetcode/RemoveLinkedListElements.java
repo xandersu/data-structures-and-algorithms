ab6f77abd7723c63a32b7a37f405c79a69bec174
xandersu <xandersu666@gmail.com>
2018-09-26 22:02:58 +0800

    leetcode 203

---
+++ src/main/java/com/xandersu/datastructuresandalgorithms/leetcode/RemoveLinkedListElements.java
@@ -0,0 +1,23 @@
+package com.xandersu.datastructuresandalgorithms.leetcode;
+
+/**
+ * @Author: suxun
+ * @Date: 2018/9/26 22:00
+ * @Description:
+ */
+public class RemoveLinkedListElements {
+
+    public ListNode removeElements(ListNode head, int val) {
+
+
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
