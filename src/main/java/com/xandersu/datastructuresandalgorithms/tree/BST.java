92726cadc6f78172cc57df31da602a9bb6b92f6f
suxun <suxun@gotokeep.com>
2018-09-27 22:54:52 +0800

    add tree 遍历 p1

--- src/main/java/com/xandersu/datastructuresandalgorithms/tree/BST.java
+++ src/main/java/com/xandersu/datastructuresandalgorithms/tree/BST.java
@@ -4,6 +4,10 @@ package com.xandersu.datastructuresandalgorithms.tree;
  * @Author: suxun
  * @Date: 2018/9/27 21:59
  * @Description: 二分搜索树
+ *               遍历就是把所有节点都访问一遍
+ *               访问的原因与业务有关
+ *               在线性结构下，遍历是容易的，树下不难
+ *
  */
 public class BST<E extends Comparable<E>> {
 
