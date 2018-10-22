b8ca9a7a294987bfc36d083c462bda80c245114a
xandersu <xandersu666@gmail.com>
2018-10-22 22:16:24 +0800

    add 线段树

--- src/main/java/com/xandersu/datastructuresandalgorithms/tree/MySegmentTree.java
+++ src/main/java/com/xandersu/datastructuresandalgorithms/tree/MySegmentTree.java
@@ -1,5 +1,8 @@
 package com.xandersu.datastructuresandalgorithms.tree;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author: suxun
  * @Date: 2018/10/21 17:12
@@ -12,6 +15,77 @@ package com.xandersu.datastructuresandalgorithms.tree;
  * --------A[0-3]-----------A[4-7]
  * ----A[0-1]  A[2-3]  A[4-5] A[6-7]
  * ---A[0]-A[1]-A[2]-A[3]-A[4]-A[5]-A[6]-A[7]
+ * ==========
+ * 平衡二叉树： 最大的深度和最小的深度差不超过一
+ * 完全二叉树一定是平衡二叉树
+ * 线段树不是完全二叉树
+ * 线段树是平衡二叉树，堆也是平衡二叉树
+ * 平衡二叉树 搜索高效
+ * 如果区间有n个元素，数组表示最多需要多少个节点？ 4n!!! 空间换时间!!!
+ * 对满二叉树：h层 一共2^h-1个节点（大约是2^h），最后一层(h-1)层,有2^(h-1)个节点 最后一层的节点数大致等于前面所有节点之和
+ * ===========
+ * 父亲节点是左右两个子树根结点的和,叶子节点的和是他自己
  */
-public interface MySegmentTree {
+public class MySegmentTree<E> {
+    private E[] data;
+    private E[] tree;
+    private Merger<E> merger;
+
+    public MySegmentTree(E[] arr, Merger<E> merger) {
+        data = (E[]) new Object[arr.length];
+        this.merger = merger;
+        for (int i = 0; i < arr.length; i++) {
+            data[i] = arr[i];
+        }
+        tree = (E[]) new Object[4 * arr.length];
+        buildSegmentTree(0, 0, data.length - 1);
+    }
+
+    /**
+     * 在treeIndex的位置创建表示区间 l到r的线段树
+     * @param treeIndex
+     * @param l         区间左端电
+     * @param r         区间右端点
+     */
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+        if (l == r) {
+            tree[treeIndex] = data[l];
+            return;
+        }
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        int mid = l + (r - l) / 2;
+
+        buildSegmentTree(leftTreeIndex, l, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= data.length) {
+            throw new IllegalArgumentException("wrong index");
+        }
+        return data[index];
+    }
+
+    public int getSize() {
+        return data.length;
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    @Override
+    public String toString() {
+        List<E> res = Arrays.asList(tree);
+        return res.toString();
+    }
 }
---
+++ src/test/java/com/xandersu/datastructuresandalgorithms/TestMySegmentTree.java
@@ -0,0 +1,24 @@
+package com.xandersu.datastructuresandalgorithms;
+
+import com.xandersu.datastructuresandalgorithms.tree.MySegmentTree;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: suxun
+ * @Date: 2018/10/22 22:11
+ * @Description:
+ */
+@SpringBootTest
+public class TestMySegmentTree {
+
+    @Test
+    public void testMySegmentTree() {
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+
+        MySegmentTree<Integer> segTree = new MySegmentTree<Integer>(nums, (a, b) -> a + b);
+
+        System.out.println(segTree.toString());
+
+    }
+}
