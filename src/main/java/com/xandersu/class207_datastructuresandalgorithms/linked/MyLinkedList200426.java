3f7149fb4ffbc516ae3104f2c365543ce9ed83c0
xandersu <840595532@qq.com>
2020-04-28 15:58:10 +0800

    add 10. 重排链表 leetCode 143. 重排链表

--- src/main/java/com/xandersu/class207_datastructuresandalgorithms/linked/MyLinkedList200426.java
+++ src/main/java/com/xandersu/class207_datastructuresandalgorithms/linked/MyLinkedList200426.java
@@ -374,6 +374,61 @@ public class MyLinkedList200426 {
     }
 
 
+    //10.重排链表
+    //LeetCode 13
+    public static void reOrder(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+        ListNode middle = getMiddle10(head);
+        ListNode reverse = reverseList10(middle.next);
+        middle.next = null;
+
+        int i = 0;
+        ListNode prev = new ListNode(0);
+        while (head != null && reverse != null) {
+            if (i%2 == 0) {
+                prev.next = head;
+                head = head.next;
+            } else {
+                prev.next = reverse;
+                reverse = reverse.next;
+            }
+            i++;
+            prev = prev.next;
+        }
+        prev.next = head != null ? head : reverse;
+    }
+
+    public static ListNode reverseList10(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = temp;
+            temp = head;
+            head = next;
+        }
+        return temp;
+    }
+
+    public static ListNode getMiddle10(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
     public static void main(String[] args) {
         ListNode listNode1 = new ListNode(1);
         ListNode listNode2 = new ListNode(2);
@@ -382,13 +437,21 @@ public class MyLinkedList200426 {
         ListNode listNode5 = new ListNode(5);
         ListNode listNode6 = new ListNode(6);
         ListNode listNode7 = new ListNode(7);
+        ListNode listNode8 = new ListNode(8);
+        ListNode listNode9 = new ListNode(9);
+        ListNode listNode10 = new ListNode(10);
+        ListNode listNode11 = new ListNode(11);
 
         listNode1.next = listNode2;
         listNode2.next = listNode3;
         listNode3.next = listNode4;
         listNode4.next = listNode5;
-//        listNode5.next = listNode6;
+        listNode5.next = listNode6;
         listNode6.next = listNode7;
+        listNode7.next = listNode8;
+        listNode8.next = listNode9;
+        listNode9.next = listNode10;
+        listNode10.next = listNode11;
         System.out.println(listNode1);
 
 //        ListNode listNode = reverseList(listNode1);
@@ -404,11 +467,15 @@ public class MyLinkedList200426 {
 
 //        System.out.println(deleteDuplicates2(listNode1));
 
-        System.out.println(rotateRight(listNode1, 2));
+//        System.out.println(rotateRight(listNode1, 2));
 
 //        int length = getLength(listNode1);
 //        System.out.println(length);
 //        System.out.println(listNode1);
-    }
 
+//        System.out.println(reOrder(listNode1));
+        reOrder(listNode1);
+        System.out.println(listNode1);
+
+    }
 }
