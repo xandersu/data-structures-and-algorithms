8263a2946e31a53fd9e77f9c65d3e090bcf57271
xandersu <840595532@qq.com>
2020-04-29 13:23:33 +0800

    add 6.判断二叉树是否是平衡二叉树 110. 平衡二叉树

--- src/main/java/com/xandersu/class207_datastructuresandalgorithms/tree/MyTree200427.java
+++ src/main/java/com/xandersu/class207_datastructuresandalgorithms/tree/MyTree200427.java
@@ -109,6 +109,30 @@ public class MyTree200427 {
         return numsOfkLevelTreeNode(root.left, k) + numsOfkLevelTreeNode(root.right, k);
     }
 
+    //6.判断二叉树是否是平衡二叉树
+    //给定一个二叉树，判断它是否是高度平衡的二叉树。
+    //本题中，一棵高度平衡二叉树定义为：
+    //一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        int leftHeight = getHigh6(root.left);
+        int rightHeight = getHigh6(root.right);
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public static int getHigh6(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHigh6(root.left), getHigh6(root.right)) + 1;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
         TreeNode l1 = new TreeNode(9);
@@ -130,7 +154,9 @@ public class MyTree200427 {
 
 //        System.out.println(numsOfNoChildNode(root));
 
-        System.out.println(numsOfkLevelTreeNode(root, 4));
+//        System.out.println(numsOfkLevelTreeNode(root, 4));
+
+        System.out.println(isBalanced(root));
 
 
     }
